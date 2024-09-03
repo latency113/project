ecd9d42b5c0e6f9b50a636b78223c8e7536f850e
latency113 <loprommy55@mail.com>
2024-09-04 00:02:31 +0700

    11.5%

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,10 +36,10 @@ const Login = () => {
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-                  <p className="space-x-1 text-sm font-light text-gray-500 dark:text-gray-400">
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet?
                       <NavLink to="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                        Sign up
+                        <p>Sign up</p>
                       </NavLink>
                   </p>
               </form>
