8d8e86b1e1bfa33ca3094b45df7679b32b868c2a
Khan <loprommy55@gmail.com>
2024-09-14 13:44:40 +0700

    Final

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <Layout>
       <div className='flex justify-center'>
-          <div className='text-center p-4 text-2xl'>
+          <div className='text-center p-40 text-2xl'>
             <p>ไม่พบหน้าที่ต้องการ</p>
             <button className='bg-purple-500 rounded-full text-white font-semibold my-5 p-4'>
               <NavLink to='/'>กลับไปหน้าแรก</NavLink>
