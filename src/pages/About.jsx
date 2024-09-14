64c824f1c824cbebc5da39f32d280aa268af950b
Khan <49835781+latency113@users.noreply.github.com>
2024-09-14 14:45:43 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 const About = () => {
   return (
     <Layout>
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8 pt-20">
         <h3 className="text-3xl font-bold text-center mb-6">เกี่ยวกับเว็บไซต์ของเรา</h3>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
