be0b3dc2969c8d8d0192e9dbc5ca0b91309e2920
Khan <49835781+latency113@users.noreply.github.com>
2024-09-14 14:01:45 +0700

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,16 +5,18 @@ import { NavLink } from 'react-router-dom';
 const NotFound = () => {
   return (
     <Layout>
-      <div className='flex justify-center'>
-          <div className='text-center p-40 text-2xl'>
-            <p>ไม่พบหน้าที่ต้องการ</p>
-            <button className='bg-purple-500 rounded-full text-white font-semibold my-5 p-4 md:w-2/5'>
-              <NavLink to='/'>กลับไปหน้าแรก</NavLink>
+      <div className='flex justify-center items-center pt-20'>
+        <div className='text-center p-6 md:p-20'>
+          <p className='text-xl md:text-2xl font-semibold'>ไม่พบหน้าที่ต้องการ</p>
+          <NavLink to='/'>
+            <button className='bg-purple-500 rounded-full text-white font-semibold w-full md:w-auto my-5 p-3 md:px-6 hover:bg-purple-600 transition-all'>
+              กลับไปหน้าแรก
             </button>
-          </div> 
+          </NavLink>
+        </div> 
       </div>
     </Layout>
   )
 }
 
-export default NotFound
+export default NotFound;
